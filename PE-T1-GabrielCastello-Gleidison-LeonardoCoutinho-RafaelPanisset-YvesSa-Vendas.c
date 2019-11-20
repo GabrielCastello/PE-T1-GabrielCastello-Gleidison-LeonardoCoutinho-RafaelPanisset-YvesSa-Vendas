c7d5aef5a20ff59afbb2e05367ae88b8e14277dd
Gabriel Castello <gabriel__castello@hotmail.com>
2019-11-20 17:36:06 -0300

    up

--- PE-T1-GabrielCastello-Gleidison-LeonardoCoutinho-RafaelPanisset-YvesSa-Vendas.c
+++ PE-T1-GabrielCastello-Gleidison-LeonardoCoutinho-RafaelPanisset-YvesSa-Vendas.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 
-int genSellCode(int startNumber, int endNumber)
+int getSellsQty(int *file)
 {
+  int counter = 0;
+  while (!feof(file))
+  {
+    int sellID;
+    fscanf(file, "%i %*s %*f %*i %*i", &sellID);
+    printf("\nSellID: %i", sellID);
+    counter++;
+  }
+  fclose(file);
+  return counter;
+}
+
+// void getSellIDs(int *file)
+// {
+//   while (!feof(file))
+//   {
+//     int number;
+//     for (int i = 0; i < rows; i++)
+//     {
+//       for (int j = 0; j < columns; j++)
+//         fscanf(file, "%i", &srcMatrix[i][j]);
+//       printf("\n");
+//     }
+//   }
+//   fclose(file);
+// }
+int genSellCode()
+{
+  int startNumber = 1, endNumber = 1000;
   return rand() % endNumber + startNumber;
 }
 
@@ -15,19 +44,43 @@ int verifyFile(int *file)
   return 1;
 };
 
+int insertSellToFile(int *file, char *name[100], float sellValue, int day, int month)
+{
+  fprintf(file, "%i %s %f %i %i\n", genSellCode(), name, sellValue, day, month);
+}
+
 int main()
 {
   srand(time(NULL));
 
   /* Abre o arquivo Vendas.txt no modo leitura "r" */
-  FILE *vendasFile = fopen("Vendas.txt", "r");
-  if (!verifyFile(vendasFile))
+  // FILE *sellsFile = fopen("Vendas.txt", "r");
+  FILE *sellsFile = fopen("VendasBackup.txt", "r");
+  if (!verifyFile(sellsFile))
   {
     /* Cria o arquivo Vendas.txt no modo de escrita */
     printf("\nCriando arquivo Vendas.txt...");
-    vendasFile = fopen("Vendas.txt", "w");
-    if (verifyFile(vendasFile))
+    sellsFile = fopen("Vendas.txt", "w");
+    if (verifyFile(sellsFile))
+    {
       printf("\nArquivo Vendas.txt criado com sucesso.");
+      char name[100];
+      float value = 0;
+      int month, day;
+      printf("\nInforme o nome do vendedor: ");
+      scanf(" %s", name);
+      printf("\nInforme o valor da venda: ");
+      scanf("%f", &value);
+      printf("\nInforme o dia da venda: ");
+      scanf("%i", &day);
+      printf /* code */ ("\nInforme o mês da venda: ");
+      scanf("%i", &month);
+      insertSellToFile(sellsFile, name, value, day, month);
+    }
+  }
+  else
+  {
+    printf("\nTotal de vendas: %i", getSellsQty(sellsFile));
   }
 
   printf("\n\n");
