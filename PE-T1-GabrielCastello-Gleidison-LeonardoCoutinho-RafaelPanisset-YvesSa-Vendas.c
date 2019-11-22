88f759fbd15d1757c0411f2955fe0aebddc92011
Gabriel Castello <gabriel__castello@hotmail.com>
2019-11-21 22:10:40 -0300

    upated

--- PE-T1-GabrielCastello-Gleidison-LeonardoCoutinho-RafaelPanisset-YvesSa-Vendas.c
+++ PE-T1-GabrielCastello-Gleidison-LeonardoCoutinho-RafaelPanisset-YvesSa-Vendas.c
@@ -1,16 +1,45 @@
 #include <stdio.h>
 
-int getSellsQty(int *file)
+void getSellsIDs(int *sellsIDs, int sellsQty)
+{
+  for (int i = 0; i < sellsQty; i++)
+  {
+    printf("\nID %i: %i;", i, sellsIDs[i]);
+  }
+}
+
+void setSellsIDs(int *file, int *sellsIDs)
+{
+  int counter = 0, i = 0;
+  while (!feof(file))
+  {
+    int sellID = NULL;
+    fscanf(file, "%i %*s %*f %*f %*f", &sellID);
+    if (sellID != NULL)
+    {
+      counter++;
+      // printf("\nSellID: %i", sellID);
+      sellsIDs[i] = sellID;
+      i++;
+    }
+  }
+}
+
+int setSellsQty(int *file)
 {
   int counter = 0;
   while (!feof(file))
   {
-    int sellID;
-    fscanf(file, "%i %*s %*f %*i %*i", &sellID);
-    printf("\nSellID: %i", sellID);
-    counter++;
+    int sellID = 0;
+    fscanf(file, "%i %*s %*f %*f %*f", &sellID);
+    // fscanf(file, "%i", &sellID);
+    if (sellID != 0)
+    {
+      counter++;
+      // printf("\nSellID: %i", sellID);
+      // printf("\ncounter: %i", counter);
+    }
   }
-  fclose(file);
   return counter;
 }
 
@@ -44,15 +73,44 @@ int verifyFile(int *file)
   return 1;
 };
 
-int insertSellToFile(int *file, char *name[100], float sellValue, int day, int month)
+int genUniqSellCode(int sellsQty, int *sellsIDs)
 {
-  fprintf(file, "%i %s %f %i %i\n", genSellCode(), name, sellValue, day, month);
+  int randomSellCode, validator = 1;
+  do
+  {
+    randomSellCode = genSellCode();
+    printf("randomSellCode: %i", randomSellCode);
+    for (int i = 0; i < sellsQty; i++)
+    {
+      if (randomSellCode == sellsIDs[i])
+      {
+        validator = 0;
+      }
+    }
+  } while (validator == 0);
+  return randomSellCode;
+}
+
+int insertSellToFile(int *file, int sellsQty, int *sellsIDs)
+{
+  char name[100];
+  float value = 0;
+  int month, day;
+  printf("\nInforme o nome do vendedor: ");
+  scanf(" %s", name);
+  printf("\nInforme o valor da venda: ");
+  scanf("%f", &value);
+  printf("\nInforme o dia da venda: ");
+  scanf("%i", &day);
+  printf("\nInforme o mês da venda: ");
+  scanf("%i", &month);
+  fprintf(file, "\n%i %s %f %i %i", genUniqSellCode(sellsQty, sellsIDs), name, value, day, month);
+  fclose(file);
 }
 
 int main()
 {
   srand(time(NULL));
-
   /* Abre o arquivo Vendas.txt no modo leitura "r" */
   // FILE *sellsFile = fopen("Vendas.txt", "r");
   FILE *sellsFile = fopen("VendasBackup.txt", "r");
@@ -64,25 +122,60 @@ int main()
     if (verifyFile(sellsFile))
     {
       printf("\nArquivo Vendas.txt criado com sucesso.");
-      char name[100];
-      float value = 0;
-      int month, day;
-      printf("\nInforme o nome do vendedor: ");
-      scanf(" %s", name);
-      printf("\nInforme o valor da venda: ");
-      scanf("%f", &value);
-      printf("\nInforme o dia da venda: ");
-      scanf("%i", &day);
-      printf /* code */ ("\nInforme o mês da venda: ");
-      scanf("%i", &month);
-      insertSellToFile(sellsFile, name, value, day, month);
+      // insertSellToFile(sellsFile, name, value, day, month);
     }
   }
   else
   {
-    printf("\nTotal de vendas: %i", getSellsQty(sellsFile));
-  }
+    int sellsQty = setSellsQty(sellsFile);
+    int sellsIDs[sellsQty];
 
-  printf("\n\n");
-  return 0;
+    // printf("\nTotal de vendas: %i", sellsQty);
+    rewind(sellsFile);
+    setSellsIDs(sellsFile, sellsIDs);
+    fclose(sellsFile);
+    // getSellsIDs(sellsIDs, sellsQty);
+
+    int selectedOption;
+    printf("\nBem-vindo ao software de vendas !");
+    do
+    {
+      selectedOption = 0;
+
+      printf("\nSelecione um dos items do menu: ");
+      printf("\n######################################################################################################");
+      printf("\n# - (1) Inserir uma venda (o código da venda não pode ser repetido)                                  #");
+      printf("\n# - (2) Remover uma venda pelo código da venda                                                       #");
+      printf("\n# - (3) Listar na tela todas as vendas de um vendedor pesquisando pelo nome do vendedor              #");
+      printf("\n# - (4) Listar na tela todas as vendas de um vendedor em um determinado dia ou mês                   #");
+      printf("\n# - (5) Imprimir na tela o valor total de vendas de um vendedor em um determinado dia ou mês         #");
+      printf("\n# - (6) Sair da execução do algoritmo                                                                #");
+      printf("\n######################################################################################################");
+      printf("\n");
+      scanf("%i", &selectedOption);
+      switch (selectedOption)
+      {
+      case 1:
+        sellsFile = fopen("VendasBackup.txt", "a");
+        insertSellToFile(sellsFile, sellsQty, sellsIDs);
+        break;
+      case 2:
+        // removeSales(vendasFile, &counter, codeSales);
+        break;
+      case 3:
+        // listSallersNames(vendasFile);
+        break;
+      case 4:
+        // listSallersDay(vendasFile);
+        break;
+      case 5:
+        // printSallersValue(vendasFile);
+        break;
+      case 6:
+        printf("Ate logo, volte sempre!! \n");
+        break;
+      }
+    } while (selectedOption != 6);
+    return 0;
+  }
 }
